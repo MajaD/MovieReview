af7dc17d1b3ea453e5e061a296cd4c2deb800c91
MajaD <Dark-Angel93@wp.pl>
2016-05-21 15:02:56 +0200

    add show page with movie description

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,27 @@
 class MoviesController < ApplicationController
 	
+	before_action :find_movie, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@movies = Movie.all.order("created_at DESC")
 	end
 
+	def show
+		
+	end 
+	
 	def new
 		@movie = Movie.new
 	end
 
 	def create
 		@movie = Movie.new(movie_params)
+
+		if @movie.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
 	end
 
 	private
@@ -16,5 +29,9 @@ class MoviesController < ApplicationController
 	def movie_params
 		params.require(:movie).permit(:title, :director, :description)
 	end
-	
+
+	def find_movie
+		@movie = Movie.find(params[:id])
+	end
+
 end
