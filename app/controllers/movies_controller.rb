679829c617835c8dad26372fd4e0d1043df9fc2f
MajaD <Dark-Angel93@wp.pl>
2016-05-21 14:38:31 +0200

    add new movie form template

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,20 @@
 class MoviesController < ApplicationController
+	
 	def index
 	end
+
+	def new
+		@movie = Movie.new
+	end
+
+	def create
+		@movie = Movie.new(movie_params)
+	end
+
+	private
+
+	def movie_params
+		params.require(:movie).permit(:title, :director, :description)
+	end
+	
 end
