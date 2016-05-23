75f39578b54a557429de66f62d488d69b02f6945
MajaD <drzewickamaja1@gmail.com>
2016-05-23 10:13:43 +0000

    create template for reviews model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522101616) do
+ActiveRecord::Schema.define(version: 20160523100451) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20160522101616) do
     t.string   "movie_poster_content_type"
     t.integer  "movie_poster_file_size"
     t.datetime "movie_poster_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
