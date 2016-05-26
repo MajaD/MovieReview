d73aa0cdbdcc2bc79d83a203440136c0e4e5dc17
MajaD <drzewickamaja1@gmail.com>
2016-05-26 18:44:11 +0000

    add some data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  {email: "angelofdeath@example.pl", encrypted_password: "$2a$10$4pJl65a5WO9tMl3M95y7teBT2wFUBGx5rMhG4FvpA4Q0SeLR38Yn6", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 6, current_sign_in_at: "2016-05-25 13:10:04", last_sign_in_at: "2016-05-25 16:50:05", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
+])
+
+Movie.create!([
+{title: "Nietykalni", director: "Olivier NakacheEric Toledano", description: "Sparaliżowany milioner zatrudnia do opieki młodego chłopaka z przedmieścia, który właśnie wyszedł z więzienia.", user_id: 1, category_id: 2},
+{title: "Zielona mila", director: "Frank Darabont", description: "Emerytowany strażnik więzienny opowiada przyjaciółce o niezwykłym mężczyźnie, którego skazano na śmierć za zabójstwo dwóch 9-letnich dziewczynek.", user_id: 1, category_id: 3},
+ ])
+
+Category.create!([
+  {name: "Akcja"},
+  {name: "Biograficzny"},
+  {name: "Dramat"},
+  {name: "Fantasy"},
+  {name: "Gangsterski"},
+  {name: "Horror"},
+  {name: "Komedia"},
+  {name: "Przygodowy"},
+  {name: "Sci-Fi"},
+  {name: "Sensacyjny"},
+  {name: "Wojenny"}
+])
